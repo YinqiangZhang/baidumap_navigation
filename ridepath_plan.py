e6208c53b1c20823d985baeee88e0420ef12eeaa
Yinqiang Zhang <yinqiang.zhang@tum.de>
2022-01-20 21:38:19 +0800

    change inputs coordinates to raw data format (WGS84)

--- ridepath_plan.py
+++ ridepath_plan.py
@@ -12,15 +12,19 @@ class  RidePlaner(object):
         self.header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
 
     def calculate_path(self, origin_gps, destination_gps):
-        url = "http://api.map.baidu.com/direction/v2/riding?origin={}&destination={}&ak={}".format(
+        url = "http://api.map.baidu.com/direction/v2/riding?&coord_type=wgs84&origin={}&destination={}&ak={}".format(
             origin_gps, destination_gps, self.api_key
         )
         html = requests.get(url, headers = self.header)
 
         html_dir = json.loads(html.text, encoding='utf-8')['result']['routes'][0]['steps']
-
+        path_origin_dir = json.loads(html.text, encoding='utf-8')['result']['routes'][0]['originLocation']
+        path_destination_dir = json.loads(html.text, encoding='utf-8')['result']['routes'][0]['destinationLocation']
+        transformed_path_origin_gps = '{},{}'.format(path_origin_dir['lng'], path_origin_dir['lat'])
+        transformed_path_destination_gps = '{},{}'.format(path_destination_dir['lng'], path_destination_dir['lat'])
+        
         origin_lngs, origin_lats = [], []
-        markers_gps = ''
+        markers_gps = '{};'.format(transformed_path_origin_gps)
         for i in range(len(html_dir)):
             step_info = html_dir[i]
             origin_lng = step_info['stepOriginLocation']['lng']
@@ -36,7 +40,8 @@ class  RidePlaner(object):
                 markers_gps += paths[j]
                 markers_gps += ';'
 
-        markers_gps += destination_gps.split(',')[1] + ',' + destination_gps.split(',')[0]
+        # TODO: test different value of origin and destination in the output results
+        markers_gps += transformed_path_destination_gps
         center_lng = np.mean(np.array(origin_lngs))
         center_lat = np.mean(np.array(origin_lats))
 
@@ -58,13 +63,10 @@ if __name__ == "__main__":
     # origin_gps = '22.60808431791728,114.00273340573993'
     # destination_gps = '22.599153684226555,114.00588269527528'
     
-    # data on the map
-    # origin_gps = '22.287783,114.149596' # 22.287783,114.149596
-    # destination_gps = '22.287081,114.150763' # 22.287081,114.150763
-    
     # data from GPS 
-    origin_gps = '22.2844866,114.1381188'
-    destination_gps = '22.2835193,114.1381264'
+    # For inputs, WGS84 coordinate is necessary here
+    origin_gps = '22.284486,114.138118'
+    destination_gps = '22.283519,114.138126'
     
     planner = RidePlaner()
     center_lng, center_lat, markers_gps = planner.calculate_path(origin_gps, destination_gps)
